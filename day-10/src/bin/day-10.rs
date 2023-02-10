389375626f19c1c1d0584482d63ad89dc2e6c2ee
andrei <andrei.reanu@gmail.com>
2023-02-10 22:05:09 +0200

    Day10 done

--- day-10/src/bin/day-10.rs
+++ day-10/src/bin/day-10.rs
@@ -5,7 +5,12 @@ use std::fs;
 fn main() {
     let file_contents = fs::read_to_string("input.txt").expect("Error reading file");
     let score1 = part_one(&file_contents);
-    let score2 = part_two(&file_contents);
+    let crt = part_two(&file_contents);
     println!("{}", score1);
-    println!("{}", score2);
+    for i in 0..crt.len() {
+        for j in 0..crt[0].len() {
+            print!("{}", crt[i][j]);
+        }
+        println!();
+    }
 }
--- day-10/src/lib.rs
+++ day-10/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 pub fn part_one(s: &str) -> isize {
     let mut cycle: isize = 0;
     let mut v: isize = 1;
@@ -43,8 +45,58 @@ pub fn part_one(s: &str) -> isize {
         .sum()
 }
 
-pub fn part_two(s: &str) -> usize {
-    0
+pub fn part_two(s: &str) -> Vec<Vec<char>> {
+    let mut cycle: usize = 0;
+    let mut v: isize = 1;
+    let mut crt: Vec<Vec<char>> = vec![vec!['*'; 40]; 6];
+    let cycles: Vec<usize> = vec![40, 80, 120, 160, 200, 240];
+    let mut cycles_index = 0;
+    s.lines().for_each(|line| {
+        let command: Vec<&str> = line.split(' ').collect();
+        match command[0] {
+            "noop" => {
+                let range: Range<usize> = cycle..cycle + 1;
+                for idx in range {
+                    if cycles.contains(&idx) {
+                        cycles_index += 1;
+                    }
+                    let sprite_center = v;
+                    let idx_compact = (idx % 40) as isize;
+                    if sprite_center == idx_compact
+                        || sprite_center - 1 == idx_compact
+                        || sprite_center + 1 == idx_compact
+                    {
+                        crt[cycles_index][idx % 40] = '#';
+                    } else {
+                        crt[cycles_index][idx % 40] = '.';
+                    }
+                }
+                cycle += 1;
+            }
+            "addx" => {
+                let range = cycle..cycle + 2;
+                for idx in range {
+                    if cycles.contains(&idx) {
+                        cycles_index += 1;
+                    }
+                    let sprite_center = v;
+                    let idx_compact = (idx % 40) as isize;
+                    if sprite_center == idx_compact
+                        || sprite_center - 1 == idx_compact
+                        || sprite_center + 1 == idx_compact
+                    {
+                        crt[cycles_index][idx % 40] = '#';
+                    } else {
+                        crt[cycles_index][idx % 40] = '.';
+                    }
+                }
+                cycle += 2;
+                v += command[1].parse::<isize>().unwrap();
+            }
+            _ => {}
+        };
+    });
+    crt
 }
 
 #[cfg(test)]
@@ -205,15 +257,153 @@ noop";
 
     #[test]
     fn test_part_two() {
-        let input = "R 5
-U 8
-L 8
-D 3
-R 17
-D 10
-L 25
-U 20";
+        let input = "addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
         let result = part_two(&input);
-        assert_eq!(result, 36);
+        assert_eq!(result, [['#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.'], ['#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.'], ['#', '#', '#', '#', '.', '.', '.', '.', '#', '#', '#', '#', '.', '.', '.', '.', '#', '#', '#', '#', '.', '.', '.', '.', '#', '#', '#', '#', '.', '.', '.', '.', '#', '#', '#', '#', '.', '.', '.', '.'], ['#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.'], ['#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#'], ['#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.']]);
     }
 }
